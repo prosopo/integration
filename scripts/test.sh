9b4291113f3f161e09658b3a6b1b521e55f1c1dc
seht <seht@hyx.net>
2022-06-02 11:14:54 +0100

    tests passing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,6 @@
 ./scripts/start-substrate.sh --test-db --restart-chain
 ./scripts/start-db.sh --env-file=.env.test
-CONTAINER_NAME=$(docker ps -q -f name=provider-api)
-docker cp .database_accounts.json $CONTAINER_NAME:/usr/src/database_accounts.json
-docker exec -ti $CONTAINER_NAME sh -c "yarn workspace @prosopo/provider run test:mock"
+cp .database_accounts.json database_accounts.json
+yarn workspace @prosopo/provider run test:mock
 ./scripts/start-substrate.sh
 ./scripts/start-db.sh
